a98783f94070213086a1554711eb76dafdbb911c
Abhinay Patthi <abhi.patthi@gmail.com>
2018-12-24 14:38:40 -0500

    Find the first missing positive number

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -1,40 +1,48 @@
+/*Problem Description:
+	Given an array of integers, find the first missing positive integer in linear time and constant space. 
+	In other words, find the lowest positive integer that does not exist in the array. The array can contain 
+	duplicates and negative numbers as well.
+
+	For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+*/
+
 class FirstMissingPositive{
 	
-	static int firstMissingPositive(int[] inputArray){
-		int arrayLength = inputArray.length;
-		int val;
-		int nextVal;
-		for(int index=0; index<arrayLength; index++){
-			val = inputArray[index];
-
-			if(inputArray[index] <= 0 || inputArray[index] > arrayLength){
-				continue;
-			}
-
-			while(inputArray[val-1] != val){
-				nextVal = inputArray[val-1];
-				inputArray[val-1] = val;
-				val = nextVal;
-				if(val<=0 || val > arrayLength){
-					break;
-				}
-			}
-		}
-
-
-		for(int index=0; index<arrayLength; index++){
-			if(inputArray[index] != index+1){
-				return index+1;
-			}
-		}
-
-		return arrayLength+1;
-	}
-
 	public static void main(String[] args){
 
 		int[] inputArray = {2,3,7,6,8,-1,-10,15};
-		System.out.println(firstMissingPositive(inputArray));
+		//int[] inputArray = {1,2,3,6,8,1,2};
+		for(int index = 0; index < inputArray.length; index++){
+			while(inputArray[index] != index + 1){
+
+				//do not swap if the value is out of range
+
+				if(inputArray[index] <= 0 || inputArray[index] >= inputArray.length){
+					break;
+				}
+
+				//check for duplicate
+				if(inputArray[index] == inputArray[inputArray[index] - 1]){
+					break;
+				}
+
+				int temp = inputArray[index];
+				inputArray[index] = inputArray[temp-1];
+				inputArray[temp - 1] = temp;
+			}
+		}
+
+		int smallestMissingNumber = 0;
+
+		for(int index = 0; index < inputArray.length; index++){
+			if(inputArray[index] != index + 1){
+				smallestMissingNumber = index + 1;
+				break;
+			}
+		}
+
+		System.out.println(smallestMissingNumber);
 	}	
 }
 
