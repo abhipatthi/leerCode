2876c15a69d0d57d45ab1bda95980cbef947ce60
Shivdarshan Devadiga <shivadarshan2212@gmail.com>
2024-10-14 17:16:52 +0530

    Create MergeKSortedLists.java
    Solved Merge k Sorted Lists problem in leetcode(question 23)

---
+++ MergeKSortedLists.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode merge(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+        int mid = start + (end - start) / 2;
+        ListNode left = merge(lists, start, mid);
+        ListNode right = merge(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        current.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
